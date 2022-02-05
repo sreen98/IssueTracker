e8f2db7669d28bacbc94111e5206e4daaad6909b
sreen98 <sreenath.premnath@gmail.com>
2022-02-05 11:13:10 +0530

    About Page refactored

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
+import styled from "styled-components";
+
+const Tittle = styled.h1`
+  text-align: left;
+  color: rgb(6, 50, 56);
+  font-family: Georgia, sans-serif;
+  font-weight: bold;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+const AboutContainer = styled(Container)`
+  margintop: 1rem;
+`;
+
+const aboutMessage =
+  "This Application is used to track the status of the issues raised";
 
 const About = () => {
-    return (
-        <Container style={{marginTop:"1rem"}}>
-            <Row>
-                <Col>
-                    <h1 className="mainheading">About</h1>
-                    <p>
-                        This Application is used to track the status of the issues raised
-                    </p>
-                </Col>
-            </Row>
-        </Container>
-    )
-
-}
+  return (
+    <AboutContainer>
+      <Row>
+        <Tittle>About</Tittle>
+      </Row>
+      <Row>
+        {aboutMessage}
+      </Row>
+    </AboutContainer>
+  );
+};
 
 export default withRouter(About);
